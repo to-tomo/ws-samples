cd78a5ae1801619e89fa0d63b2702ed623fdc13d
to-tomo <110224497+to-tomo@users.noreply.github.com>
2022-08-04 07:32:47 +0900

    外部から Watson Studio Cloud の ジョブを実行 (#1)

---
+++ scripts/external_execution/main.py
@@ -0,0 +1,146 @@
+"""
+外部から Watson Studio Cloud のジョブを実行
+"""
+
+import logging
+import requests
+import schedule
+import sys
+import time
+
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+def generate_bearer_token(api_key: str) -> str:
+    """
+    ベアラートークン生成
+    MEMO: https://cloud.ibm.com/docs/account?topic=account-iamtoken_from_apikey&interface=api  # noqa: E501
+    """
+
+    url = "https://iam.cloud.ibm.com/oidc/token"
+    headers = {"Content-Type": "application/x-www-form-urlencoded"}
+    payload = {
+        "apikey": api_key,
+        "grant_type": "urn:ibm:params:oauth:grant-type:apikey"
+        }
+    # IAM トークン取得
+    response = requests.post(
+        url,
+        headers=headers,
+        data=payload,
+    ).json()
+    iam_token = response["access_token"]
+    return f"Bearer {iam_token}"
+
+
+def get_job_id(bearer_token: str, project_id: str, job_name: str) -> str:
+    """
+    Job ID 取得
+    MEMO: https://cloud.ibm.com/apidocs/watson-data-api-cpd#jobs-list
+    """
+
+    url = f"https://api.dataplatform.cloud.ibm.com/v2/jobs?project_id={project_id}"  # noqa: E501
+    headers = {
+        "Authorization": bearer_token,
+        "Content-Type": "application/json"
+        }
+    # Job list 取得
+    response = requests.get(
+        url,
+        headers=headers
+    ).json()
+    # Job ID(=asset_id) 取得
+    return next(
+        v["metadata"]["asset_id"]for v in response["results"] if v["metadata"]["name"] == job_name  # noqa: E501
+    )
+
+
+def run_job(bearer_token: str, job_id: str, project_id: str) -> str:
+    """
+    Job 実行
+    MEMO: https://cloud.ibm.com/apidocs/watson-data-api#job-runs-create
+    """
+
+    url = f"https://api.dataplatform.cloud.ibm.com/v2/jobs/{job_id}/runs?project_id={project_id}"  # noqa: E501
+    headers = {
+        "Authorization": bearer_token,
+        "Content-Type": "application/json"
+        }
+    # Job 実行
+    response = requests.post(
+        url,
+        headers=headers
+    ).json()
+    # Run ID(=asset_id) 取得
+    return response["metadata"]["asset_id"]
+
+
+def check_job_status(
+        bearer_token: str, job_id: str, run_id: str, project_id: str
+        ) -> str:
+    """
+    Job ステータス確認
+    MEMO: https://cloud.ibm.com/apidocs/watson-data-api#job-runs-get
+    """
+
+    url = f"https://api.dataplatform.cloud.ibm.com/v2/jobs/{job_id}/runs/{run_id}?project_id={project_id}"  # noqa: E501
+    heders = {
+        "Authorization": bearer_token,
+        "Content-Type": "application/json"
+        }
+    # Job ステータス取得
+    response = requests.get(
+        url,
+        headers=heders
+    ).json()
+    return response["entity"]["job_run"]["state"]
+
+
+def job():
+    # ご自身の環境に合わせて修正
+    api_key = "<your api key>"
+    job_name = "<your job name>"
+    project_id = "<your project id>"
+    bearer_token = generate_bearer_token(api_key)
+    logger.info("Bearer token has been generated.")
+    job_id = get_job_id(bearer_token, project_id, job_name)
+    logger.info(f"Job ID is [{job_id}].")
+    run_id = run_job(bearer_token, job_id, project_id)
+    logger.info(f"Run ID is [{run_id}].")
+    retury_times = 12  # JOB ステータス確認リトライ回数
+    retry_sleep = 6    # JOB ステータス確認 sleep 期間(秒)
+    try:
+        for i in range(retury_times):
+            job_status = check_job_status(
+                bearer_token, job_id, run_id, project_id
+                )
+            logger.info(f"Job status is [{job_status}].")
+            # Job 完了確認
+            if job_status == "Completed":
+                logger.info("Completed.")
+                return
+            time.sleep(retry_sleep)
+            # タイムアウト確認
+            if i == retury_times-1:
+                raise TimeoutError()
+    except TimeoutError:
+        logger.exception("Timeout error.")
+        raise
+    except Exception:
+        logger.exception("Unexpected exception.")
+        raise
+
+
+if __name__ == "__main__":
+    try:
+        logger.info("Job is working...")
+        # 1分毎に Job を実行
+        schedule.every(1).minutes.do(job)
+        while True:
+            schedule.run_pending()
+            time.sleep(1)
+    except Exception as e:
+        logger.exception("Errors occurred.", e)
+        sys.exit(1)
